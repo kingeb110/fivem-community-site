c8a47a892a642e0145b9d5c3ea48422d21658dbd
King EB110 <91443258+kingeb110@users.noreply.github.com>
2022-07-24 15:47:29 -0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,78 @@
+<?php
+
+//////////////////////////////////
+//     KING EB110 DEVELOPMENT   //        
+//  https://kingeb110.com/store //
+//////////////////////////////////
+
+/// IMPORTANT ///
+// ONLY CHANGE THE TEXT I SIDE THE " " DO NOT DELETE ANY ; OR ANY " " //
+// IF YOU NEED ANY HELP MAKE A TICKET IN https://discord.gg/XZPsrNpzsW //
+
+// Main site settings //
+
+//your website's URL
+$domain = "https://products.kingeb110.com/free-encrypted/"; // keep the / at the end of the link
+
+//Your community name. Ex. Rapid Roleplay
+$cname = "Community Name"; 
+
+//Your communities slogan
+$cslogan = "A very good community"; 
+
+//your communities description. Not used in site, used in embeds.
+$cabout = "A very good community"; 
+
+//background image that will be used in the website.  a link to an image or a gif.
+$backgroundimg = "https://media.discordapp.net/attachments/962580562076389406/1000871929466593300/background.png";
+
+// community logo.  a link to an image or a gif.
+$clogo = "https://media.discordapp.net/attachments/962580562076389406/997771643390795796/kinglo.png";
+
+// This part is very important so please read this before chabging anything!!!
+// Replace the IPHERE text with your ip. Ex. 194.156.89.32
+// Then replace the PORTHERE with your ip's port. Ex. 30120
+// That is ALL you need to change. Do not change anything else
+$file = file_get_contents('http://IPHERE:PORTHERE/dynamic.json');
+
+$buttname = "Join";
+$bttnlink = "https://discord.gg/";
+
+// Gallery
+// Recomended size for the images is 1000x350 pixels.  a link to an image or a gif.
+$gimg1 = "https://cdn.discordapp.com/attachments/962580562076389406/1000604074909106176/freerpic1.png"; // Image 1 must be 1000x350 pixels.  a link to an image or a gif.
+$gimg2 = "https://cdn.discordapp.com/attachments/962580562076389406/1000604075425009684/freerpic2.png"; // Image 2 must be 1000x350 pixels.  a link to an image or a gif.
+$gimg3 = "https://cdn.discordapp.com/attachments/962580562076389406/1000604075940913244/freerpic3.png"; // Image 3 must be 1000x350 pixels.  a link to an image or a gif.
+// Gallery end //
+
+// Team page //
+
+// Image 1 must be 500x333 pixels. Alink to an image or a gif.
+$mem1pic = "https://media.discordapp.net/attachments/962580562076389406/1000609591945613362/team1.png"; 
+// Name of the first member
+$mem1name = "Elon Musk";
+// Rank title of the first member
+$mem1tile = "Owner";
+// description of the first member
+$mem1desc = "some description here";
+
+// Image 1 must be 500x333 pixels. A link to an image or a gif.
+$mem2pic = "https://media.discordapp.net/attachments/962580562076389406/1000609591945613362/team1.png"; 
+// Name of the second member
+$mem2name = "Musk Elon";
+// Rank title of the second member
+$mem2tile = "Co Owner";
+// description of the second member
+$mem2desc = "some description here";
+
+// Image 1 must be 500x333 pixels. A link to an image or a gif.
+$mem3pic = "https://media.discordapp.net/attachments/962580562076389406/1000609591945613362/team1.png"; 
+// Name of the third member
+$mem3name = "Elon Musk II";
+// Rank title of the third member
+$mem3tile = "Moderator";
+// description of the third member
+$mem3desc = "some description here";
+// Team page end //
+
+?>
